4c991c72434c9cef53a1b73aedb16fcad86c671e
Rushi Shah <2016rshah@gmail.com>
2016-04-01 01:14:33 -0400

    #rebrand

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Commit.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :commit,
+    [app: :comet,
      version: "0.0.1",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
