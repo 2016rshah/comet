8a6f068038341e4950132a8cf76588f7a22fac1d
Rushi Shah <2016rshah@gmail.com>
2016-04-01 00:19:07 -0400

    Started messing with the github api

--- lib/commit_web.ex
+++ lib/commit_web.ex
@@ -25,6 +25,25 @@ defmodule Commit.Web do
 		|> send_resp(200, "hello world")
 		|> halt
 	end
+
+	def filterToNames(list) do
+		Enum.map list, fn x -> Dict.get x, "name" end
+	end
+
+	def filterToMostRecentCommits(list) do
+		Enum.map list, fn repo -> {(Dict.get repo, "message"), (Dict.get repo, "message")} 
+	end
+
+	get "username/:name" do
+		repos = filterToNames (Tentacat.Repositories.list_users(name))
+		repo_commits = Enum.map repos, fn repo -> Tentacat.Commits.list(name, repo) end
+		commits = filterToMostRecentCommits repo_commits
+		#might be able to pattern match stuff like this:
+		#http://elixir-lang.org/getting-started/keywords-and-maps.html#maps
+		conn
+		|> send_resp(200, "Hello there #{latest_commits}")
+		|> halt
+	end
 	
 	get _ do
 		conn
