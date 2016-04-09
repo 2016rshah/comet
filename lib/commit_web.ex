37dfe3f3878fecc44add6f8e68feafb64c588789
Rushi Shah <2016rshah@gmail.com>
2016-04-09 17:34:30 -0400

    Defined port number for heroku

--- lib/commit_web.ex
+++ lib/commit_web.ex
@@ -1,11 +1,20 @@
 defmodule Commit.Web do
 
+	def portNumber do
+		p = System.get_env("PORT")
+		if p do
+			p 
+		else
+			4000
+		end
+	end
+
 	def init(options) do
 	  options
 	end
 
 	def start_link do
-	  {:ok, _} = Plug.Adapters.Cowboy.http Commit.Controller, []
+	  {:ok, _} = Plug.Adapters.Cowboy.http Commit.Controller, [], port: portNumber
 	end
 
 end  
