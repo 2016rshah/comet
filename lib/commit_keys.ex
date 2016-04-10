876464bbcd93265763b69f40f790b4c164432228
Rushi Shah <2016rshah@gmail.com>
2016-04-10 16:35:58 -0400

    Clarified for other users

--- lib/commit_keys.ex
+++ lib/commit_keys.ex
@@ -2,7 +2,6 @@ defmodule Commit.Keys do
 	def github_key do
 		System.get_env("GITHUB_ACCESS_KEY")
 		#Don't forget to do this in bash first
-		#$ export VARNAME="my value" 
-		
+		#$ export GITHUB_ACCESS_KEY="my value" 
 	end
 end
